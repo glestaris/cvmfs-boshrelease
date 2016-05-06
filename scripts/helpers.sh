a3d13e279281e5a2898b05d3611117b6971cca29
George Lestaris <glestaris@gmail.com>
2016-05-06 17:53:33 +0100

    Remove test dir from CVMFS source tree

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 gfd() {
+  set -x
+  [ -d src/cvmfs/test ] && rm -Rf src/cvmfs/test
   bosh create release --force
   bosh upload release
   bosh deployment manifests/bosh-lite.yml
   bosh deploy --recreate
+  set +x
 }
